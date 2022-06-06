eda027f8b4dbf101381ce6c8ed9c142a68f3aa9f
Ninzore <33434617+Ninzore@users.noreply.github.com>
2022-06-06 15:47:46 +0100

    Feat: 定时清理间隔选项

--- src/plugins/sourcedown/config.py
+++ src/plugins/sourcedown/config.py
@@ -1,8 +1,8 @@
 from pydantic import BaseSettings
 
-
 class Config(BaseSettings):
     remote_drv: str = '填写rclone远程文件夹名称'
+    auto_clean_interval = 10
 
     class Config:
         extra = "ignore"
