cff02c0a281992029f495cb0f74c82e65da31472
Ninzore <33434617+Ninzore@users.noreply.github.com>
2022-06-09 18:00:06 +0100

    Feat: subprocess watchdog

--- src/plugins/sourcedown/utils.py
+++ src/plugins/sourcedown/utils.py
@@ -1,5 +1,7 @@
 import re
+import time
 import asyncio
+import subprocess
 
 from nonebot import get_bot
 from nonebot.adapters.onebot.v11.adapter import Bot, Message, MessageSegment
@@ -33,3 +35,17 @@ def timestampProc(ts: str):
     if match is None:
         return None
     return match.group(0).replace('：', ':')
+
+def subProcWatchdog(proc: subprocess.Popen):
+    while True:
+        code = proc.poll()
+        if code == 0:
+            print('子进程完成')
+            return
+        elif code == None:
+            pass
+        else:
+            proc.terminate()
+            raise RuntimeError('出错啦', proc.args)
+            break
+        time.sleep(1)
