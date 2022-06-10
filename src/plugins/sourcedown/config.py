3f4b8d4b49a18e792c0d820b88589d92c295d56a
Ninzore <33434617+Ninzore@users.noreply.github.com>
2022-06-10 10:19:31 +0100

    Update config.py

--- src/plugins/sourcedown/config.py
+++ src/plugins/sourcedown/config.py
@@ -6,6 +6,7 @@ class Config(BaseSettings):
     remote_drv: str = '填写rclone远程文件夹名称'
     dl_root = os.path.join(Path(__file__).parent.parent.parent.parent.resolve(), 'temp')
     auto_clean_interval = 10
+    mount_fld = '填写rclone mount的本地文件夹'
 
     class Config:
         extra = "ignore"
